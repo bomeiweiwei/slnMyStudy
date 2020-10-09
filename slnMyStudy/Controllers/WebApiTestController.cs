05b1671296bcc199da9d4b922369ba5a8422b91a
DESKTOP-R9CU97T\bomei <bomeilonch0210@gmail.com>
2020-10-09 13:36:28 +0800

    Axios && Fetch

--- slnMyStudy/Controllers/WebApiTestController.cs
+++ slnMyStudy/Controllers/WebApiTestController.cs
@@ -16,5 +16,21 @@ namespace slnMyStudy.Controllers
             ViewBag.baseUrl = baseUrl;
             return View();
         }
+
+        public ActionResult TestAxios() 
+        {
+            string baseUrl = new Uri(Request.Url, Url.Content("~")).AbsoluteUri;
+            baseUrl = baseUrl.Remove(baseUrl.Length - 1);
+            ViewBag.baseUrl = baseUrl;
+            return View();
+        }
+
+        public ActionResult TestFetch()
+        {
+            string baseUrl = new Uri(Request.Url, Url.Content("~")).AbsoluteUri;
+            baseUrl = baseUrl.Remove(baseUrl.Length - 1);
+            ViewBag.baseUrl = baseUrl;
+            return View();
+        }
     }
 }
